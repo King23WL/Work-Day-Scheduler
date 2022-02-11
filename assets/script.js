ce5361f46a4e8672906635cb7eb34ebcabd4b632
Xuanan Yue <xuananyue@Xuanans-MBP.attlocal.net>
2022-02-11 14:25:52 -0800

    add js files adjust html

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+var saveBtn = $(".saveBtn");
+
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
+function listColor(){
+    var hour=moment().hours();
+    $(".time-block").each(function(){
+        var currentHour = parseInt($(this).attr("id"));
+        if (currentHour>hour){
+            $(this).addClass("future");
+        }else if (currentHour === hour){
+            $(this).addClass("present");
+        }else{
+            $(this).addClass("past");
+        }
+    })
+};
+
+saveBtn.on("click", function(){
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan").val();
+    localStorage.setItem(time, plan);
+
+});
+
+function starter(){
+
+    $(".hour").each(function(){
+        var currentHour=$(this).text();
+        var currentPlan=localStorage.getItem(currentHour);
+        if(currentHour!==null){
+            $(this).siblings(".plan").val(currentPlan);
+        }
+
+
+    });
+
+};
+
+listColor();
+starter();
